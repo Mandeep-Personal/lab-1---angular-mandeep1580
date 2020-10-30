17c213d103d46f59bbd643c9b8a7235177e4b31f
mandeep1580 <mandeep15@gmail.com>
2020-10-30 15:06:31 -0700

    updated components - navbar, header banner, jumbotron and search bar

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { ProductItemsComponent } from './product-items/product-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainNavComponent
+    MainNavComponent,
+    ProductItemsComponent
   ],
   imports: [
     BrowserModule
